df6bf20cc0186dc6165caf4b691fab8e59de9e35
antonvinod <seliksamai@gmail.com>
2015-05-01 17:18:59 -0700

    Update mouse.ino

--- mouse.ino
+++ mouse.ino
@@ -2,12 +2,12 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 //These are the assigned pins for the teensy 3.1
-#define NW_EMIT 		      20
-#define N_EMIT 		      19
-#define NE_EMIT 		      20
-#define NW_RECV 		      23
-#define N_RECV 		      21
-#define NE_RECV 		      22
+#define NW_EMIT               20
+#define N_EMIT                         19
+#define NE_EMIT               20
+#define NW_RECV               23
+#define N_RECV                       21
+#define NE_RECV               22
 
 //#define W_EMIT                19
 //#define E_EMIT                19
@@ -30,16 +30,123 @@
 #define DEBUG_PRINT     1
 
 enum {
-	IR_LEFT = 0,
+    IR_LEFT = 0,
         IR_RIGHT,
         IR_MID,
-	NUM_IRLED
+    NUM_IRLED
 } number_of_IRled;
 
 //led connected to pins 3 and 4
 //const int Led1 = 20;   //this is for left and right leds
-//const int Led2 = 21;	//this is for middle led
+//const int Led2 = 21;    //this is for middle led
 //Proportinoal control for mouse (l = left, r = right etc...)
+
+
+//////////////////////////////////////////////////
+/*        NEWLY ADDED CODE        */
+//////////////////////////////////////////////////
+
+volatile unsigned long leftTick = 0;
+volatile unsigned long rightTick = 0;
+
+//velocity of each wheel in meters per millisecond
+const double rightVelocity = 0;
+const double leftVelocity = 0;
+
+//current speed values
+double currentrightSpeed = 0;
+double currentleftSpeed = 0;
+
+//derivative motor control variables
+unsigned long prev_encRight = 0;
+unsigned long prev_encLeft = 0;
+unsigned long diff_encRight = 0;
+unsigned long diff_encLeft = 0;
+double velocity_encRight = 0;
+double velocity_encLeft = 0;
+double velocity_errorRight = 0;
+double velocity_errorLeft = 0;
+double set_velocity = 60;
+double Kd_speed = .125;
+
+int l_wheel_ticks = 102;
+int r_wheel_ticks = 19;
+int l_turn_ticks = 0;
+int r_turn_ticks = 0;
+////////////////////////////////////////////////// *****KProportional ONLY***********//////////////////////
+//PWM values translated from velocity
+double Kp_speed = .6;
+int rightspeed = 40;
+int leftspeed = 40;
+
+
+
+const int RencoderA = 14;
+const int LencoderA = 16;
+const int rightPWMA = 5;
+const int rightPWMB = 6;
+const int leftPWMA = 3;
+const int leftPWMB = 4;
+
+
+
+void set_ticks(void){
+ 
+ l_wheel_ticks = 102;
+ r_wheel_ticks = 19;
+ l_turn_ticks = leftTick + l_wheel_ticks;
+ r_turn_ticks = rightTick + r_wheel_ticks;
+ //right_turn();
+}
+
+void stopboth_motors(void) {
+   analogWrite(leftPWMA,0);
+   analogWrite(leftPWMB,0);
+   analogWrite(rightPWMA,0);
+   analogWrite(rightPWMB,0);
+}
+//may need to switch the left_speed and right_speed
+void move_forward(unsigned char leftspeed, unsigned char rightspeed) {
+        analogWrite(leftPWMA,leftspeed);
+    analogWrite(leftPWMB,0);
+    analogWrite(rightPWMA,rightspeed);
+    analogWrite(rightPWMB,0);
+    //digitalWrite(Led1,LOW);
+    //digitalWrite(Led2,HIGH);
+}
+void move_backward(unsigned char leftspeed, unsigned char rightspeed){
+        analogWrite(leftPWMA,0);
+    analogWrite(leftPWMB,leftspeed);
+    analogWrite(rightPWMA,0);
+    analogWrite(rightPWMB,rightspeed);
+    //digitalWrite(Led1,LOW);
+    //digitalWrite(Led2,HIGH);
+}
+void turn_right(unsigned char leftspeed, unsigned char rightspeed) {
+    analogWrite(leftPWMA,leftspeed);
+    analogWrite(leftPWMB,0);
+    analogWrite(rightPWMA,0);
+    analogWrite(rightPWMB,rightspeed);
+        //digitalWrite(Led1,HIGH);
+    //digitalWrite(Led2,LOW);
+}
+void turn_left(unsigned char leftspeed, unsigned char rightspeed) {
+    analogWrite(leftPWMA,0);
+    analogWrite(leftPWMB,leftspeed);
+    analogWrite(rightPWMA,rightspeed);
+    analogWrite(rightPWMB,0);
+        //digitalWrite(Led1,LOW);
+    //digitalWrite(Led2,HIGH);
+}
+//////////////////////////////////////////////////
+/*        END OF NEW CODE            */
+//////////////////////////////////////////////////
+
+bool left_empty = false;
+bool right_empty = false;
+
+int no_l_wall = 0;
+int no_r_wall = 0;
 
 
 int kp_left = 0;
@@ -60,25 +167,20 @@ int err_sum_right = 0;
 int Output_right = 0;
 int Offset_right = 100;
 
-
-char x_max = 5;
-char y_max = 5;
-char cell_array [5][5];
-int no_l_wall = 100;//was 50
-int no_r_wall = 70;
-
-int l_speed = 40 *.9;//was 50
-int r_speed = 37 *.9;//was 45
+//int leftspeed = 40 *.9;//was 50
+//int rightspeed = 37 *.9;//was 45
 
 int l_error = 0;
 int r_error = 0;
 int m_error = 0;
 
+/*
 //motor driver logic pins
 const int leftPWMA = 3;
 const int leftPWMB = 4;
 const int rightPWMA = 5;
 const int rightPWMB = 6;
+*/
 
 //global variables
 int ir_raw[3]={0, 0, 0}; //this initializes each arr elem to 0
@@ -91,6 +193,7 @@ unsigned long timer = 0;
 psuedo code for speed control
 
 unsigned long ENC = 0
+
 unsigned long prev_ENC = 0
 
 long dif_ENC = ENC - prev_ENC
@@ -103,133 +206,177 @@ error_val = set_val - speed_one
 
 speed_control = kp*error_val
 
-
-
-
-
 */
 
+/*
+void check_empty(){
+   
+    if(//sensor left is really low)
+    {   
+        left_empty = true;
+    }
+    if(//sensor right is really low)
+    {
+        right_empty = true;
+    }
+   
+}
+*/
+
+// Encoder functions
+void rightENCODER(void){
+  rightTick++; 
+}
 
 
+void leftENCODER(void){
+  leftTick++;
+}
+
+void resetValues(void){
+  stopboth_motors();
+  rightTick = 0;
+  leftTick = 0;
+  prev_encRight = 0;
+  prev_encLeft = 0;
+}
+
+
+//RIGHT TURNING
+void right_turn(void){
+ stopboth_motors();
+ //int left_turntick = leftTick;
+ //int right_turntick = rightTick;
+
+ //turn_right(rightSpeed, leftSpeed);
+ set_ticks();
+ while(leftTick < l_turn_ticks){
+ 
+   //leftTick++;
+   //rightTick++;
+   if(leftTick > l_turn_ticks) {stopboth_motors();}
+   Serial.print("\nleftTick is: ");
+   Serial.print(leftTick);
+   Serial.print("\nl_turn_ticks is: ");
+   Serial.print(l_turn_ticks);
+   Serial.print("\nrightTick is: ");
+   Serial.print(rightTick);
+   Serial.print("\nr_turn_Ticks is: ");
+   Serial.print(r_turn_ticks);  
+  turn_right(rightspeed, leftspeed);
+ }
+ move_forward(0,0);
+ delay(1000);
+ 
+}
+
+//LEFT TURNING
+void left_turn(void){
+ stopboth_motors();
+ //int left_turntick = leftTick;
+ //int right_turntick = rightTick;
+
+ //turn_right(rightSpeed, leftSpeed);
+ set_ticks();
+ while(rightTick < r_turn_ticks){
+ 
+   //leftTick++;
+   //rightTick++;
+   if(rightTick > r_turn_ticks) {stopboth_motors();}
+   Serial.print("\nleftTick is: ");
+   Serial.print(rightTick);
+   Serial.print("\nl_turn_ticks is: ");
+   Serial.print(r_turn_ticks);
+   Serial.print("\nrightTick is: ");
+   Serial.print(leftTick);
+   Serial.print("\nr_turn_Ticks is: ");
+   Serial.print(l_turn_ticks);  
+  turn_left(rightspeed, leftspeed);
+ }
+ move_forward(0,0);
+ delay(1000);
+ 
+}
+
+
+
+void checkVelocity(void){
+
+  diff_encRight = rightTick - prev_encRight;
+ 
+  prev_encRight = rightTick;
+ 
+  velocity_encRight = (diff_encRight/.001);
+ 
+  velocity_errorRight = set_velocity - velocity_encRight;
+ 
+  rightspeed = (Kd_speed*velocity_errorRight);
+ 
+}
 
 void switch_sensors() {
-	switch(led_state) {
-		case 0:
+    switch(led_state) {
+        case 0:
                         //ACTIONS
-			ir_raw[0] = analogRead(NW_RECV);
-			ir_raw[2] = analogRead(NE_RECV);
+            ir_raw[0] = analogRead(NW_RECV);
+            ir_raw[2] = analogRead(NE_RECV);
 
-			digitalWrite(N_EMIT,HIGH);
-			digitalWrite(NW_EMIT,LOW);
-			digitalWrite(NE_EMIT,LOW);
+            digitalWrite(N_EMIT,HIGH);
+            digitalWrite(NW_EMIT,LOW);
+            digitalWrite(NE_EMIT,LOW);
                      //   digitalWrite(W_EMIT,HIGH);
                      //   digitalWrite(E_EMIT,HIGH);
-			if (DEBUG_PRINT) {
+            if (DEBUG_PRINT) {
 
-				Serial.print("\nCase 0:  ");
-				Serial.print("\n         ---->left recv: ");
+                Serial.print("\nCase 0:  ");
+                Serial.print("\n         ---->left recv: ");
                                 Serial.print(ir_raw[0]);
                                 Serial.print("<---->mid recv: ");
-				Serial.print(ir_raw[1]);
+                Serial.print(ir_raw[1]);
                                 Serial.print("---->right recv: ");
-				Serial.print(ir_raw[2]);
+                Serial.print(ir_raw[2]);
                                 Serial.print("\nWest recv: ");
                                 Serial.print(sides[0]);
                                 Serial.print("<--------->East recv: ");
                                 Serial.print(sides[1]);
                                 //delay(500);
-			}
+            }
                         //TRANSIOTIONS
                         led_state = 1;
-			break;
-		case 1:
+            break;
+        case 1:
                         //ACTIONS
-			ir_raw[1] = analogRead(N_RECV);
+            ir_raw[1] = analogRead(N_RECV);
               //          sides[0] = analogRead(W_RECV);
               //          sides[1] = analogRead(E_RECV);
-		        digitalWrite(N_EMIT,LOW);
-			digitalWrite(NW_EMIT,HIGH);
-			digitalWrite(NE_EMIT,HIGH);
+                digitalWrite(N_EMIT,LOW);
+            digitalWrite(NW_EMIT,HIGH);
+            digitalWrite(NE_EMIT,HIGH);
                 //        digitalWrite(W_EMIT, LOW);
                 //        digitalWrite(E_EMIT,LOW);
-			if (DEBUG_PRINT) {
-				Serial.print("\nCase 1:  ");
+            if (DEBUG_PRINT) {
+                Serial.print("\nCase 1:  ");
                                 Serial.print("\n       ---->left recv: ");
                                 Serial.print(ir_raw[0]);
                                 Serial.print("<---->mid recv: ");
-				Serial.print(ir_raw[1]);
+                Serial.print(ir_raw[1]);
                                 Serial.print("---->right recv: ");
-				Serial.print(ir_raw[2]);
+                Serial.print(ir_raw[2]);
                                 Serial.print("\nWest recv: ");
                                 Serial.print(sides[0]);
                                 Serial.print("<--------->East recv: ");
                                 Serial.print(sides[1]);
                                 //delay(500);
-			}
+            }
                         //TRANSITIONS
                         led_state = 0;
-			break;
-		default: break;
-	}
+            break;
+        default: break;
+    }
 }
-void maze(){
 
- char curr_distance = 1;
-   for(char x = 1; x <= x_max; x++){
 
-   }
-   for(char y = 1; y <= y_max; y++){
-
-   }
-
-   int addr;
-   if (addr == 512){
-      addr = 0;
-   }
-
-}
-void stopboth_motors(void) {
-   analogWrite(leftPWMA,0);
-   analogWrite(leftPWMB,0);
-   analogWrite(rightPWMA,0);
-   analogWrite(rightPWMB,0);
-}
-void move_forward(unsigned char left_speed, unsigned char right_speed) {
-        analogWrite(leftPWMA,left_speed);
-	analogWrite(leftPWMB,0);
-	analogWrite(rightPWMA,right_speed);
-	analogWrite(rightPWMB,0);
-	//digitalWrite(Led1,LOW);
-	//digitalWrite(Led2,HIGH);
-}
-void move_backward(unsigned char left_speed, unsigned char right_speed){
-        analogWrite(leftPWMA,0);
-	analogWrite(leftPWMB,left_speed);
-	analogWrite(rightPWMA,0);
-	analogWrite(rightPWMB,right_speed);
-	//digitalWrite(Led1,LOW);
-	//digitalWrite(Led2,HIGH);
-}
-void turn_right(unsigned char left_speed, unsigned char right_speed) {
-	analogWrite(leftPWMA,left_speed);
-	analogWrite(leftPWMB,0);
-	analogWrite(rightPWMA,0);
-	analogWrite(rightPWMB,right_speed);
-        //digitalWrite(Led1,HIGH);
-	//digitalWrite(Led2,LOW);
-}
-void turn_left(unsigned char left_speed, unsigned char right_speed) {
-	analogWrite(leftPWMA,0);
-	analogWrite(leftPWMB,left_speed);
-	analogWrite(rightPWMA,right_speed);
-	analogWrite(rightPWMB,0);
-        //digitalWrite(Led1,LOW);
-	//digitalWrite(Led2,HIGH);
-}
 void move_robot() {
-        //added to get the error for each detector
-        //l_error = ir_raw[0] - L_SET;
-        //r_error = ir_raw[2] - R_SET;
 
         error_left = ir_raw[0] - NW_SET;
         err_sum_left += error_left;
@@ -246,111 +393,110 @@ void move_robot() {
         kd_right = 2 * (error_right - prev_err_right);
 
         Output_right = (kp_right + ki_right +kd_right)/3;
-
-        l_error = 16;
-        r_error = 10;
-  	if(ir_raw[1] < FRONT_WALL_DETECT){
+//////////////////////////////////////       
+        //l_error = 16;
+        //r_error = 10;
+//////////////////////////////////////
+      if(ir_raw[1] < FRONT_WALL_DETECT){
             //go forward if we dont have a wall in front
-            move_forward(l_speed, r_speed);
+            move_forward(leftspeed, rightspeed);
             //proportional control
             if(error_left > 30){
-            //if((ir_raw[0] + Offset - ir_raw[2]) > 20){
-            //if(ir_raw[0] > LEFT_WALL_STRGHT){    //too close to left
-              move_forward(l_speed + Output_left, r_speed);
+
+              move_forward(leftspeed + Output_left, rightspeed);
             }
             //else if((ir_raw[0] + Offset - ir_raw[2]) < 0){
 
             if(error_right > 30){
             //else if(((ir_raw[0] + Offset - ir_raw[2]) < -20)){    //too close to right
-              move_forward(l_speed, r_speed + Output_right);
+              move_forward(leftspeed, rightspeed + Output_right);
             }
 
             else{
-                  //move_forward(l_speed,r_speed);      //CHANGED SPEED (LEFT, RIGHT) WAS (128,128)
 
-                  //if(ir_raw[0] < no_l_wall && ir_raw[2] < no_r_wall  && ir_raw[1] < 150){
-                  //   move_forward(l_speed, r_speed);
-                  //}
                   if (ir_raw[0] < no_l_wall && ir_raw[2] > no_r_wall ){
                     Serial.print("                  GOING LEFT");
+                     //move forward x number of cells and
+                     //call go left instead of this stuff
                      delay(150);
                      stopboth_motors();
                      delay(100);
-                     move_forward(l_speed, r_speed);
+                     move_forward(leftspeed, rightspeed);
                      delay(250);
-                     //turn_left(l_speed, r_speed);
+                     //turn_left(leftspeed, rightspeed);
                      delay(400);
-                     move_forward(l_speed, r_speed);
+                     move_forward(leftspeed, rightspeed);
                      delay(350);
                   }
                   else if (ir_raw[0] > no_l_wall && ir_raw[2] < no_r_wall ){
                      Serial.print("                  GOING RIGHT");
+                     //move forward x number of cells and
+                     //call go right instead of this stuff
                      delay(150);
                      stopboth_motors();
                      delay(100);
-                     move_forward(l_speed, r_speed);
+                     move_forward(leftspeed, rightspeed);
                      delay(250);
-                     turn_right(l_speed, r_speed);
+                     turn_right(leftspeed, rightspeed);
                      delay(400);
-                     move_forward(l_speed, r_speed);
+                     move_forward(leftspeed, rightspeed);
                      delay(350);
                   }
-                //else{
-                //    move_forward(l_speed, r_speed);
-                //}
+
             }
 
            prev_err_left = error_left;
            prev_err_right = error_right;
-	}
+    }
         else if(ir_raw[1] >= FRONT_WALL_DETECT){
                     Serial.print("                  DETECT FRONT WALL");
+                   
+                    //do two turn rights or call 180
                     stopboth_motors();
                     delay(5000);
-                    move_backward(l_speed, r_speed);
+                    move_backward(leftspeed, rightspeed);
                     delay(500);
-                    //delay(3000);
-                    //turn_right(l_speed * 0.8, r_speed * 0.8);
-                    //delay(3000);
-                    //move_forward(l_speed, r_speed);
-	}
+
+    }
 }
 void setup() {
-  Serial1.begin(9600);
+  Serial.begin(9600);
+ 
+ //IR sensor emitter and receiver setup
   pinMode(NW_EMIT,OUTPUT);
-  pinMode(N_EMIT,OUTPUT);
-  pinMode(NE_EMIT,OUTPUT);
   pinMode(NW_RECV,INPUT);
+
+  pinMode(N_EMIT,OUTPUT);
   pinMode(N_RECV,INPUT);
+ 
+  pinMode(NE_EMIT,OUTPUT);
   pinMode(NE_RECV,INPUT);
- // pinMode(W_EMIT, OUTPUT);
- // pinMode(E_EMIT, OUTPUT);
- // pinMode(W_RECV, INPUT);
- // pinMode(E_RECV, INPUT);
-  //init sensors
+
+//IR digital write setup
   digitalWrite(NW_EMIT,HIGH);
   digitalWrite(N_EMIT,LOW);
   digitalWrite(NE_EMIT,HIGH);
- // digitalWrite(W_EMIT,LOW);
- // digitalWrite(E_EMIT,LOW);
+
+//Encoder Setup
+  pinMode(RencoderA,INPUT);
+  pinMode(LencoderA,INPUT);
+  attachInterrupt(14,rightENCODER,RISING);
+  attachInterrupt(16,leftENCODER,RISING);
   stopboth_motors();
 }
+
 void loop() {
-	timer = micros();
-	Serial.print("\nswitch begin");
+    timer = micros();
+    Serial.print("\nswitch begin");
         //delay(500);
-	switch_sensors();
-	//delay(500);
+    switch_sensors();
+    //delay(500);
         Serial.print("\nswitch ended");
 
-	//k_pl = 5 * l_error;
-      //  k_pm = 5 * m_error;
-      //  k_pr = 5 * r_error;
-
-	Serial.print("\nmove begin");
+    Serial.print("\nmove begin");
         //delay(500);
-	move_robot();
-	//delay(500);
+    move_robot();
+    //delay(500);
         Serial.print("\nmove ended");
-	while((micros()-timer)<1000) {delayMicroseconds(250);}	//was i microsecond
+    while((micros()-timer)<1000) {delayMicroseconds(250);}    //was i microsecond
 }
